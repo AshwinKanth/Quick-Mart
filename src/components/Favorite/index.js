38ac3a99e6a524c96b15e92a7b4a56bdccc51205
AshwinKanth <ashwinvjrj@gmail.com>
2024-11-20 14:38:34 +0530

    Add Favorite

--- src/components/Favorite/index.js
+++ src/components/Favorite/index.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../Header";
 import FavoriteItem from "../FavoriteItem";
 import AppContext from "../../Context/AppContext";
@@ -7,21 +8,40 @@ import "./index.css"
 const Favorite = () => (
     <AppContext.Consumer>
         {value => {
-            const { isDarkTheme,favoriteList } = value
+            const { isDarkTheme, favoriteList } = value
             const homeTheme = isDarkTheme ? 'dark' : 'light'
+
+            const showEmptyView = favoriteList.length === 0
 
             return (
                 <>
                     <Header />
                     <div className={`fav-container ${homeTheme}`}>
-                        <div>
-                            <h1 className='fav-heading'>Favourites</h1>
-                            <ul className='fav-list'>
-                                {favoriteList.map(eachFavItem => (
-                                    <FavoriteItem key={eachFavItem.id} favProductData={eachFavItem} />
-                                ))}
-                            </ul>
-                        </div>
+                        {showEmptyView ? (
+                            <div className="fav-empty-view-container">
+                                <img
+                                    src="https://res.cloudinary.com/dq1ktqbtb/image/upload/v1732093519/FavLogo_1_sv5zrk.png"
+                                    className="fav-empty-img"
+                                    alt="fav empty"
+                                />
+                                <h1 className="fav-empty-heading">Your Favorites Is Empty</h1>
+
+                                <Link to="/">
+                                    <button type="button" className="exploreButton">
+                                        Explore
+                                    </button>
+                                </Link>
+                            </div>
+                        ) : (
+                            <div>
+                                <h1 className='fav-heading'>Favorites</h1>
+                                <ul className='fav-list'>
+                                    {favoriteList.map(eachFavItem => (
+                                        <FavoriteItem key={eachFavItem.id} favProductData={eachFavItem} />
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
                     </div>
                 </>
             )
