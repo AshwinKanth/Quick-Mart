8bf8d02dae9f7055dd12e0e452f907cb8b127d0f
AshwinKanth <ashwinvjrj@gmail.com>
2025-08-15 15:05:15 +0530

    Updated Filter images and Login background

--- src/components/ProductCategoryItem/index.js
+++ src/components/ProductCategoryItem/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import "./index.css"
 
-const ProductCategoryItem = (props) => {
-  const { productCategoryData } = props
-  const { name,slug} = productCategoryData
+const ProductCategoryItem = ({ productCategoryData }) => {
+  const { name, slug } = productCategoryData
+
+  const imageFileName = name + '.jpg'
+
+  const imageSrc = `/assets/${imageFileName}`
 
   return (
     <Link to={`/products/category/${slug}`} className='categoryItem'>
       <li className='categoryItem'>
+        <img
+          src={imageSrc}
+          alt={name}
+          className='categoryImage'
+        />
         <p className='categoryName'>{name}</p>
       </li>
     </Link>
